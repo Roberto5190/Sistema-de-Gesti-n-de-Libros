a77028f0576f7680c2815dc30ffa863e9c4faa2c
Roberto Fernández <rfernandez5190@esdmadrid.es>
2025-05-18 00:31:13 +0200

    added helpers functions to inventario class -> inventario.py

--- backend/models/inventario.py
+++ backend/models/inventario.py
@@ -22,14 +22,52 @@ class Inventario:
             raise ValueError('Libro no encontrado')
         
         # Verificamos si hay suficiente stock
-        if libro.cantidad < cantidad:
+        if libro.stock < cantidad:
             raise ValueError('No hay suficiente stock para esta venta')
         
         # Restamos la cantidad del stock
-        libro.cantidad -= cantidad
+        libro.stock -= cantidad
         print(f"Venta registrada: {cantidad} ejemplares de '{libro.titulo}' vendidos.")
 
         # Log de la venta (puede ser una lista o simplemente imprimir en consola)
         print(f"Venta registrada en el log: {cantidad} ejemplares de '{libro.titulo}'.")
 
 
+
+    #  -- HELPERS ----------------------------------------
+    # -- LISTA LIBROS DEL INVENTARIO --
+    def listar_libros(self) -> list[dict]:
+        """
+        Lista todos los libros en el inventario.
+
+        Recorre todos los Libro almacenados y devuelve una lista de diccionarios,
+        usando el método to_dict() de cada libro.
+
+        Returns:
+            list: Lista de libros en el inventario.
+        """
+        return [libro.to_dict() for libro in self._libros.values()]
+    
+    # -- LIBROS LENGTH --
+    def __len__(self):
+        """
+        Devuelve la cantidad de libros en el inventario.
+
+        Returns:
+            int: Cantidad de libros en el inventario.
+        """
+        return len(self._libros)
+
+    # -- LIBROS ITERATOR --
+    def __iter__(self):
+        """
+        Permite iterar sobre los libros en el inventario.
+        Devuelve un iterador sobre las instancias de Libro.
+
+        ej: for libro in inv:
+                print(libro.titulo, libro.stock)
+
+        return:
+            Libro: Cada libro en el inventario.
+        """
+        return iter(self._libros.values())
